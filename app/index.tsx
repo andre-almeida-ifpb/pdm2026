2851818a6d1a501ae43884b5a22f5d0ddb5899cc
André Atanasio <andre.almeida@ifpb.edu.br>
2026-03-17 11:21:10 -0300

    Teste TextInput

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [nome, setNome] = useState('');
+
   return (
     <View
       style={{
         flex: 1,
+        padding: 16,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput 
+        placeholder="Digite algo ..."
+        onChangeText={ novoTexto => setNome(novoTexto) }
+        value={nome}
+        style={{
+          width: "100%",
+          padding: 5,
+          borderWidth: 1
+        }}
+      />
+      <Text>{nome}</Text>
     </View>
   );
 }
